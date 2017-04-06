d60003d6e74c94f01aede4c4a1c58c0e98204ff1
Nekeniehl <Nekeniehl@users.noreply.github.com>
2017-04-06 14:56:30 +0200

    Added EdiSegmentGroupTests (#37)
    * Added unit test for EdiSegmentGroupTests
    
    * Delete paket.dependencies

---
+++ test/indice.Edi.Tests/EdiSegmentGroupTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+using indice.Edi.Tests.Models;
+
+using Xunit;
+
+namespace indice.Edi.Tests
+{
+    public class EdiSegmentGroupTests
+    {
+        [Fact]
+        public void ReferenceSegment ()
+        {
+            var grammar = EdiGrammar.NewEdiFact();
+            var interchange = default(ORDRSP);
+
+            using (var stream = Helpers.GetResourceStream("ordrsp.edi"))
+            {
+                interchange = new EdiSerializer().Deserialize<ORDRSP>(new StreamReader(stream), grammar);
+
+                Assert.NotNull(interchange.ListNachricht.First().Referenz_der_Anfrage.ReferenzderAnfrage);
+            }
+        }
+    }
+}
