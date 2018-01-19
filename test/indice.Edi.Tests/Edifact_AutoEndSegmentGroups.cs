b1f6bf27031a009151633642a19e7c3353eb53d5
Christian Käser <ck@hebrech.de>
2018-01-19 14:47:21 +0100

    Test AutoEndSegmentGroups

---
+++ test/indice.Edi.Tests/Edifact_AutoEndSegmentGroups.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using indice.Edi.Tests.Models;
+using Xunit;
+
+namespace indice.Edi.Tests
+{
+    public class Edifact_AutoEndSegmentGroups
+    {
+        [Fact]
+        [Trait(Traits.Tag, "EDIFact")]
+        [Trait(Traits.Issue, "#75")]
+        public void AutoEndSegmentGroups() {
+            var grammar = EdiGrammar.NewEdiFact();
+            var interchange = default(AutoEndSegmentGroups);
+
+            using (var stream = Helpers.GetResourceStream("edifact.AutoEndSegmentGroups.edi")) {
+                var serializer = new EdiSerializer {AutoEndSegmentGroups = true};
+                interchange = serializer.Deserialize<AutoEndSegmentGroups>(new StreamReader(stream), grammar);
+            }
+
+            Assert.Equal(1, interchange.Messages.Count);
+
+            var message = interchange.Messages.First();
+            Assert.Equal(message.Group.Id, "Message1.Group");
+            Assert.NotNull(message.Group.Element1);
+            Assert.Equal(message.Group.Element1.Id, "Message1.Group.Element1");
+            Assert.NotNull(message.Group.Element2);
+            Assert.Equal(message.Group.Element2.Id, "Message1.Group.Element2");
+            Assert.NotNull(message.AfterGroup);
+            Assert.Equal(message.AfterGroup.Id, "Message1.AfterGroup");
+        }
+    }
+}
---
+++ test/indice.Edi.Tests/Models/AutoEndSegmentGroups.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using indice.Edi.Serialization;
+
+namespace indice.Edi.Tests.Models
+{
+    class AutoEndSegmentGroups
+    {
+        public List<Message> Messages { get; set; }
+
+        [EdiMessage]
+        public class Message
+        {
+            public Group Group { get; set; }
+
+            public AfterGroup AfterGroup { get; set; }
+        }
+
+        [EdiSegmentGroup("GRP")]
+        public class Group
+        {
+            [EdiValue("X(35)", Path = "GRP/0")]
+            public string Id { get; set; }
+
+            public InGroup1 Element1 { get; set; }
+
+            public InGroup2 Element2 { get; set; }
+        }
+
+        [EdiPath("IG1")]
+        [EdiSegment]
+        public class InGroup1
+        {
+            [EdiValue("X(35)", Path = "IG1/0")]
+            public string Id { get; set; }
+        }
+
+        [EdiPath("IG2")]
+        [EdiSegment]
+        public class InGroup2
+        {
+            [EdiValue("X(35)", Path = "IG2/0")]
+            public string Id { get; set; }
+        }
+
+        [EdiPath("AFT")]
+        [EdiSegment]
+        public class AfterGroup
+        {
+            [EdiValue("X(35)", Path = "AFT/0")]
+            public string Id { get; set; }
+        }
+    }
+}
