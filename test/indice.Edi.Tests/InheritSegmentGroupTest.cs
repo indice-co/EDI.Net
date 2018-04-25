98c34113b54ccf9797dc46fc83e06a40ad421b7a
Malte Bitter <malte.bitter@bit-engineering.com>
2018-04-25 14:11:35 +0200

    Add unit test for inheriting a SegmentGroup from a Segment

---
+++ test/indice.Edi.Tests/InheritSegmentGroupTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using indice.Edi.Tests.Models;
+using Xunit;
+
+namespace indice.Edi.Tests
+{
+    public class InheritSegmentGroupTest
+    {
+        [Fact]
+        public void InheritSegmentGroup() {
+            var grammar = EdiGrammar.NewEdiFact();
+            var interchange = new InheritSegmentGroup();
+            interchange.Messages = new List<Models.InheritSegmentGroup.Message>();
+            var msg = new InheritSegmentGroup.Message();
+            msg.Id = "Group1";
+            msg.Element = new Models.InheritSegmentGroup.InGroup();
+            msg.Element.Id = "Element1";
+            interchange.Messages.Add(msg);
+
+            string output = null;
+            using (var writer = new System.IO.StringWriter()) {
+                new EdiSerializer().Serialize(writer, grammar, interchange);
+                output = writer.ToString();
+            }
+
+            var GrpPos = output.IndexOf("Group1");
+            var ElmPos = output.IndexOf("Element1");
+
+            Assert.True(GrpPos >= 0);
+            Assert.True(ElmPos >= 0);
+            Assert.True(GrpPos < ElmPos);
+        }
+    }
+}
---
+++ test/indice.Edi.Tests/Models/InheritSegmentGroup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using indice.Edi.Serialization;
+
+namespace indice.Edi.Tests.Models
+{
+    class InheritSegmentGroup
+    {
+        public List<Message> Messages { get; set; }
+
+        [EdiSegment(), EdiPath("GRP")]
+        public class GRP
+        {
+            [EdiValue("X(35)", Path = "GRP/0")]
+            public string Id { get; set; }
+        }
+
+        [EdiSegment, EdiPath("IN1")]
+        public class InGroup
+        {
+            [EdiValue("X(35)", Path = "IN1/0")]
+            public string Id { get; set; }
+        }
+
+        [EdiSegmentGroup("GRP")]
+        public class Message : GRP
+        {
+            public InGroup Element { get; set; }
+        }
+    }
+}
