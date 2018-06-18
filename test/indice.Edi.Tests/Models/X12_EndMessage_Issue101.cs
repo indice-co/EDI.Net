7c8d24901d574d7b11eab79b3fadf264bb4c84b6
richardwalenga <39505959+richardwalenga@users.noreply.github.com>
2018-06-18 04:39:52 -0400

    Fixing the issues I have had mapping values from SE to the class I have designated as EdiMessage (#102)

---
+++ test/indice.Edi.Tests/Models/X12_EndMessage_Issue101.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using indice.Edi.Serialization;
+
+namespace indice.Edi.Tests.Models
+{
+    public class Interchange_Issue101
+    {
+        public Message Msg { get; set; }
+
+        [EdiMessage]
+        public class Message
+        {
+            [EdiValue("9(4)", Path = "ST/1")]
+            public string HeaderControl { get; set; }
+            
+            public Return Return { get; set; }
+            
+            [EdiValue("9(4)", Path = "SE/1")]
+            public string TrailerControl { get; set; }
+        }
+		
+		[EdiSegmentGroup("TFS")]
+		public class Return
+		{
+			public FormGroupSegment FormGroupSegment { get; set; }
+		}
+
+        [EdiSegmentGroup("FGS")]
+        public class FormGroupSegment
+        {
+            public DateSegment DateSegment { get; set; }
+            [EdiValue("X(5)", Path = "FGS/2")]
+            public string Id { get; set; }
+        }
+
+        [EdiSegment, EdiPath("DTM")]
+        public class DateSegment
+        {
+            [EdiValue("9(8)", Format = "yyyyMMdd", Path = "DTM/1")]
+            public DateTime Date { get; set; }
+        }
+    }
+}
