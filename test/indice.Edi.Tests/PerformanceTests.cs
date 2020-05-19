6b66e576572f1be98eaf9c4bb7d6c6a88cb30db2
Constantinos Leftheris <c.leftheris@indice.gr>
2020-05-19 16:42:09 +0300

    perf

--- test/indice.Edi.Tests/PerformanceTests.cs
+++ test/indice.Edi.Tests/PerformanceTests.cs
@@ -21,5 +21,16 @@ namespace indice.Edi.Tests
             Assert.Equal(78880, interchange.Messages.Count);
             Assert.NotNull(interchange);
         }
+        [Fact(Skip = "Performance"), Trait(Traits.Tag, "EDIFact"), Trait(Traits.Issue, "#154"), Trait(Traits.Tag, "Performance")]
+        public void Should_Parse_20_Megabyte_File_02() {
+            var grammar = EdiGrammar.NewEdiFact();
+            var interchange = default(EdiFact_Issue154_Interchange);
+            using (var stream = Helpers.GetBigSampleStream("edifact.Issue154.Perf02.edi")) {
+                interchange = new EdiSerializer().Deserialize<EdiFact_Issue154_Interchange>(new StreamReader(stream), grammar);
+            }
+
+            //Assert.Equal(78880, interchange.Messages.Count);
+            Assert.NotNull(interchange);
+        }
     }
 }
