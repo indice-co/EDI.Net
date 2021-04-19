0a97c34bd705836329bf07d80bf93d8ae09dbcff
Jonas Høgh <joh@cvation.com>
2021-04-19 13:22:28 +0200

    Add a test that shows the parser enters an infinite loop when given a wrong path in an EdiCondition for an Edifact message.

---
+++ test/indice.Edi.Tests/Models/EdiFact_Issue188.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using indice.Edi.Serialization;
+
+namespace indice.Edi.Tests.Models
+{
+
+    public class Interchange_Issue188
+    {
+        public IList<IFTMINMessage> IftminMessages { get; } = new List<IFTMINMessage>();
+
+        public IList<INVOICMessage> InvoiceMessages { get; } = new List<INVOICMessage>();
+
+
+        [EdiCondition("IFTMIN", Path = "UNH/2/0")]
+        [EdiMessage]
+        public class IFTMINMessage
+        {
+            public IList<DTM> DTMs { get; } = new List<DTM>();
+        }
+
+        [EdiCondition("INVOIC", Path = "UNH/2/0")]
+        [EdiMessage]
+        public class INVOICMessage
+        {
+            public IList<DTM> DTMs { get; } = new List<DTM>();
+        }
+
+        [EdiSegment, EdiPath("DTM")]
+        public class DTM
+        {
+            [EdiValue("9(3)", Path = "DTM/0/0")] public string Qualifier { get; set; }
+
+            [EdiValue("X(35)", Path = "DTM/0/1")] public string Value { get; set; }
+
+            [EdiValue("9(3)", Path = "DTM/0/2")] public string FormatQualifier { get; set; }
+        }
+    }
+}
