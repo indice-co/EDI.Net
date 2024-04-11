d2e3a6b4ad7f9b30630d38851a7f0bd40ba64304
elotoja <lukasz.budziak@masters.com.pl>
2024-04-11 10:58:11 +0200

    Add Interchange and INVOIC message

---
+++ test/indice.Edi.Tests/Issue256/Interchange.cs
@@ -0,0 +1,10 @@
+﻿using indice.Edi.Tests.Issue256.InterchangeSegments;
+
+namespace indice.Edi.Tests.Issue256.Interchanges;
+
+public class Interchange<T>
+{
+    public UNB InterchangeHeader { get; set; }
+    public T Invoice { get; set; }
+    public UNZ InterchangeTrailer { get; set; }
+}
