82bd5dc10c5126e66fb14f5461cfc8b8fb787230
elotoja <lukasz.budziak@masters.com.pl>
2024-04-11 10:57:58 +0200

    Add test

---
+++ test/indice.Edi.Tests/Issue256/Edifact_INVOICTests.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using indice.Edi.Tests.Issue256;
+using indice.Edi.Tests.Issue256.Interchanges;
+using Xunit;
+
+namespace indice.Edi.Tests
+{
+    public class Edifact_INVOICTests
+    {
+        [Fact]
+        [Trait(Traits.Tag, "EDIFact")]
+        [Trait(Traits.Issue, "#256")]
+        public void ReferenceSegment()
+        {
+            var grammar = EdiGrammar.NewEdiFact();
+            var interchange = default(Interchange<INVOIC>);
+
+            using (var stream = Helpers.GetResourceStream("edifact.Issue256.INVOIC.edi")) {
+                interchange = new EdiSerializer().Deserialize<Interchange<INVOIC>>(new StreamReader(stream), grammar);
+            }
+
+            Assert.NotNull(interchange);
+        }
+    }
+}
